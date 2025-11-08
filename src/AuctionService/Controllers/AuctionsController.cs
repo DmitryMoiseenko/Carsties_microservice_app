10d0907872036b8d2031fd4f82cbc9cd43bd50f7
Dmitry Moiseenko <dmoiseenko@ingenio.com>
2025-11-08 22:23:37 +0100

    Added "POST", "PUT" and "DELETE" HTTP methods to the AuctionsController. Removed the unnesessary "Required" attributes from the UpdateAuctionDto.

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -1,5 +1,6 @@
 using AuctionService.Data;
 using AuctionService.DTOs;
+using AuctionService.Entities;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -43,5 +44,80 @@ public class AuctionsController : ControllerBase
         }
 
         return _mapper.Map<AuctionDto>(auction);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto)
+    {
+        var auction = _mapper.Map<Auction>(auctionDto);
+
+        // TODO: add current user as seller
+        auction.Seller = "test";
+
+        _context.Auctions.Add(auction);
+
+        bool result = await _context.SaveChangesAsync() > 0;
+        if (!result)
+        {
+            return BadRequest("Could not save changes to the DB");
+        }
+
+        var auctionDtoResult = _mapper.Map<AuctionDto>(auction);
+
+        return CreatedAtAction(nameof(GetAuctionById), new { auction.Id }, auctionDtoResult);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateAuction(Guid id, [FromBody] UpdateAuctionDto auctionDto)
+    {
+        Auction auction = await _context.Auctions
+            .Include(x => x.Item)
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+        if (auction == null)
+        {
+            return NotFound();
+        }
+
+        // TODO: check that seller == username
+
+        auction.Item.Make = auctionDto.Make ?? auction.Item.Make;
+        auction.Item.Model = auctionDto.Model ?? auction.Item.Model;
+        auction.Item.Color = auctionDto.Color ?? auction.Item.Color;
+        auction.Item.Mileage = auctionDto.Mileage ?? auction.Item.Mileage;
+        auction.Item.Year = auctionDto.Year ?? auction.Item.Year;
+
+        bool result = await _context.SaveChangesAsync() > 0;
+
+        if (!result)
+        {
+            return BadRequest("Problem saving changes");
+        }
+
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteAuction(Guid id)
+    {
+        Auction auction = await _context.Auctions.FindAsync(id);
+
+        if (auction == null)
+        {
+            return NotFound();
+        }
+
+        // TODO: check that seller == username
+
+        _context.Auctions.Remove(auction);
+
+        bool result = await _context.SaveChangesAsync() > 0;
+
+        if (!result)
+        {
+            return BadRequest("Could not update DB");
+        }
+
+        return Ok();
     }
 }
